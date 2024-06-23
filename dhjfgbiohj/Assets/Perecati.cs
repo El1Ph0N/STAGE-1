3820aeeb8b6567e1547931befddd724bc62095e2
El1Ph0N <167353879+El1Ph0N@users.noreply.github.com>
2024-06-23 20:14:16 +0300

    START

---
+++ dhjfgbiohj/Assets/Perecati.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perecati : MonoBehaviour
+{
+    Rigidbody rb;
+    //public GameObject FIstcol;
+    //public GameObject Secondcol ;
+    public float spdash;
+   // public Animator animator;
+    //public Animation DAsh;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            rb.AddForce(transform.forward * spdash, ForceMode.Impulse);
+            //animator.Play("DASH");
+            //FIstcol.SetActive(false);
+            //Secondcol.SetActive(true);
+            
+        }
+
+    }
+   // private void end()
+   // {
+        
+    //}
+}
---
+++ dhjfgbiohj/Assets/movePlayer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class movePlayer : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 0.5f;
+    private Vector3 moveVector;
+    public float jump_forse = 0f;
+    bool isGrounded = true;
+    public float spdash = 10f;
+
+          
+ 
+            
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 DD = transform.TransformDirection(Vector3.down);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, DD, 1.5f))
+            {
+                rb.AddForce(transform.up * jump_forse, ForceMode.VelocityChange);
+            }
+                
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            rb.AddForce(moveVector * spdash, ForceMode.Impulse);
+            //animator.Play("DASH");
+            //FIstcol.SetActive(false);
+            //Secondcol.SetActive(true);
+
+        }
+
+    }
+    void FixedUpdate()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");        
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
+
+        
+    }
+}
